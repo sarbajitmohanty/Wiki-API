15644417dd43fddfa516b05a05b6a82b360c74e3
sarbajitmohanty <sarbajit.mohanty02@gmail.com>
2021-05-23 18:16:01 +0530

    get articles

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true, useFindAndModify: true });
+mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true, useFindAndModify: true, useUnifiedTopology: true });
 
 const articleSchema = {
     title: String,
@@ -18,6 +18,16 @@ const articleSchema = {
 };
 
 const Article = mongoose.model("Article", articleSchema);
+
+app.get("/articles", function (req, res) {
+    Article.find({}, function (err, foundArticles) {
+        if (!err) {
+            res.send(foundArticles);
+        } else {
+            res.send(err);
+        }
+    });
+});
 
 app.listen(3000, function () {
     console.log("Server started on port 3000");
