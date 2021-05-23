97009b25938a28e799eea6c3c40183b5442a9765
sarbajitmohanty <sarbajit.mohanty02@gmail.com>
2021-05-23 18:42:15 +0530

    post articles

--- app.js
+++ app.js
@@ -29,6 +29,16 @@ app.get("/articles", function (req, res) {
     });
 });
 
+app.post("/articles", function (req, res) {
+    // console.log(req.body.title);
+    // console.log(req.body.content);
+    const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content
+    });
+    newArticle.save();
+});
+
 app.listen(3000, function () {
     console.log("Server started on port 3000");
 });
