9aab8824f8a6225df41dce8f62866c69510f5332
sarbajitmohanty <sarbajit.mohanty02@gmail.com>
2021-05-23 17:47:57 +0530

    setting up mongodb

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true, useFindAndModify: true });
+
+const articleSchema = {
+    title: String,
+    content: String
+};
+
+const Article = mongoose.model("Article", articleSchema);
+
 app.listen(3000, function () {
     console.log("Server started on port 3000");
 });
